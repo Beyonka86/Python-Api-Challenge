6eccdace477e58a83dafd793546e6ad051a8be47
Beyonka Powell <beyonka86@gmail.com>
2024-02-04 20:58:11 -0600

    Final Submission

---
+++ Starter_Code/Starter_Code/starter_code/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "035b2b5f51330550991e045daac1035f"
+
+# Geoapify API Key
+geoapify_key = "522d8bda111b4c7eb1025a67b717e817"
--- Starter_Code/Starter_Code/starter_code/api_keys.py
+++ Starter_Code/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "2d7331055bf17917798122899d7ba297"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "33fd536d05014ea7997f49e627fb73fd"
